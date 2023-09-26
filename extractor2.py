9be2c637d76c9f4cbf24ac8b39b3f480418bba96
Ayron Felipe <ayron41@gmail.com>
2023-09-26 11:41:20 -0300

    feat: alterando algumas coisas

--- extractor2.py
+++ extractor2.py
@@ -1,24 +1,29 @@
 import glob
 import os
 import shutil
+
 import win32api
 
-path = input('Digite o caminho da pasta de onde copiar os arquivos: ')
-diretorio_destino = input('Diga o nome da diretório de destino: ')
-extensao = input('Diga a extensão que você deseja mover ex (.pdf): ')
-indice_pai = input('Índice da pasta com o nome da cidade: ')
-indice_filho = input('Índice da pasta com o nome da pessoa: ')
+path = input("Digite o caminho da pasta de onde copiar os arquivos: ")
+diretorio_destino = input("Diga o nome da diretório de destino: ")
+extensao = input("Diga a extensão que você deseja mover ex (.pdf): ")
+indice_pai = input("Índice da pasta com o nome da cidade: ")
+indice_filho = input("Índice da pasta com o nome da pessoa: ")
 
 os.mkdir(diretorio_destino)
 
 lol_path = "%s\\" % path
 files = [f for f in glob.glob(lol_path + "**/*.%s" % (extensao), recursive=True)]
 for file in files:
-    vish = os.path.join(diretorio_destino, file.split('\\')[int(indice_pai)], file.split('\\')[int(indice_filho)])
+    vish = os.path.join(
+        diretorio_destino,
+        file.split("\\")[int(indice_pai)],
+        file.split("\\")[int(indice_filho)],
+    )
     if not os.path.exists(vish):
         os.makedirs(vish)
     try:
-        print('OKAAY!!')
+        print("OKAAY!!")
         shutil.copy2(file, "%s\\" % vish)
     except:
         print("%s - WRONG!!!!!" % file)
@@ -29,7 +34,6 @@ for file in files:
         try:
             shutil.copy2(rum, "%s\\" % vish)
         except:
-            import pdb; pdb.set_trace()
+            breakpoint()
         else:
-            print('MAS DEU CERTO!!')
- 
+            print("MAS DEU CERTO!!")
